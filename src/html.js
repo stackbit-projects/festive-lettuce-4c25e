803e498798469dca85394d212e869ed1ee46590c
Stackbit Code Editor <projects@stackbit.com>
2021-03-24 23:32:19 +0000

    html.js: changed

--- src/html.js
+++ src/html.js
@@ -6,7 +6,7 @@ export default function HTML(props) {
     <html {...props.htmlAttributes}>
       <head>
         {props.headComponents}
-        
+        <iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/933620899&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/djdashny" title="DasH" target="_blank" style="color: #cccccc; text-decoration: none;">DasH</a> · <a href="https://soundcloud.com/djdashny/no-te-enamores-remix-farruko-x-milly-x-jay-wheeler-x-nio-garcia-intro-105bpm-djdashny-mp3" title="No te enamores (Remix) - Farruko x Milly x Jay Wheeler x Nio Garcia - Intro 105bpm - @DJDASHNY .mp3" target="_blank" style="color: #cccccc; text-decoration: none;">No te enamores (Remix) - Farruko x Milly x Jay Wheeler x Nio Garcia - Intro 105bpm - @DJDASHNY .mp3</a></div>
       </head>
       <body {...props.bodyAttributes}>
         {props.preBodyComponents}
