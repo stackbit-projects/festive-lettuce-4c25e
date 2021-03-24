443bc733dfcaa028bd7a4097db30c51b304c8a81
Stackbit Code Editor <projects@stackbit.com>
2021-03-24 23:51:44 +0000

    html.js: changed

--- src/html.js
+++ src/html.js
@@ -6,7 +6,9 @@ export default function HTML(props) {
     <html {...props.htmlAttributes}>
       <head>
         {props.headComponents}
-        
+        <audio src="/music/good_enough.mp3" controls>
+<p>If you are reading this, it is because your browser does not support the audio element.</p>
+</audio>
       </head>
       <body {...props.bodyAttributes}>
         {props.preBodyComponents}
